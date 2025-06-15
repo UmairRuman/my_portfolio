061ae26ca9ad2cd25ad30b7081c8acf0c84e4959
UmairRuman <144768916+UmairRuman@users.noreply.github.com>
2025-06-15 15:22:04 +0500

    adding updated Cv and updating link icon

--- src/components/common/ResumeModal.tsx
+++ src/components/common/ResumeModal.tsx
@@ -58,14 +58,14 @@ const ResumeModal: React.FC<ResumeModalProps> = ({
       </div>
       <div className="d-flex justify-content-center gap-3 mb-4">
         <a
-          href="public\UmairRumanCV.pdf"
+          href="public/my_cv.pdf"
           download
           className="btn button-primary resume-modal-button"
         >
           <i className="fas fa-download"></i> Download Resume
         </a>
         <a
-          href="public\UmairRumanCV.pdf"
+          href="public/my_cv.pdf"
           target="_blank"
           rel="noopener noreferrer"
           className="btn button-secondary resume-modal-button"
